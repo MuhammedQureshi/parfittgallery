e7c22b88f0b72b7a7e6909dd9957ea4ec8e660cd
LilAliTech <lilalix20@gmail.com>
2023-03-20 09:30:38 +0000

    Main

---
+++ components/about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div id='about' className="w-full md:h-screen p-2 flex items-center py-16">
+        <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+          <div className= "col-span-2">
+              <p className="uppercase text-xl tracking-widest text-[#071ff2]">About Us</p>
+              </div>
+              </div>
+              </div>
+    )
+}
+
+export default About;
